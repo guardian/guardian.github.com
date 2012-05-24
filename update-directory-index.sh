7eb475bd11536913cb7232b2d992e002b89d524e
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-05-24 17:27:18 +0100

    Release 5.13 of guardian-management
    Modified update-directory-index.sh to sort to try
    and eliminate platform differences

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -5,6 +5,7 @@ for DIR in $(find . -type d  \( ! -regex '.*/\..*' \) ); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" |\
+      sort |\
       grep -v "^\./$" |\
       grep -v "^\..*/$" |\
       grep -v "^index\.html$"|\
