7be88ed46b9506f71ca4945e70ec23a75d99d1fc
Robert Rees <robert.rees@guardian.co.uk>
2014-04-04 11:28:21 +0100

    Updates the indexing script to only work on maven and ivy folders and use the fixed top-level index page

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for DIR in $(find . -type d  \( ! -regex '.*/\..*' \) ); do
+for DIR in $(find maven ivy  -type d  \( ! -regex '.*/\..*' \) ); do
   echo -e "Creating directory listing index.html for "$DIR
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
